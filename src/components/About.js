b5456ecce9d77fecd6cc327a3071cad3e66bfe3f
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2025-10-19 16:37:14 +0530

    update projects page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './About.css'
+import StarsBackground from './StarsBackground'
 function About() {
   return (
-    <div>
+    <div className='main-bg'>
+     <StarsBackground />
       <h1>About Me</h1>
       <p>This is the about section.</p>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Projects.css";
+import StarsBackground from "./StarsBackground";
 
 function Projects() {
   return (
-    <div>
-      
+    <div className="main-bg">
+      <StarsBackground />
+      <div className="prj-heading">
+        <h3>My Recent <span>Works</span> </h3>
+        <p>Here are some of my recent projects:</p>
+      </div>
+      <div className="prj-main-pg">
+        <div className="my-prj-div"></div>
+        <div className="my-prj-div"></div>
+        <div className="my-prj-div"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
