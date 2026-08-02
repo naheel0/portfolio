9cc31d8b8784af74608c4136fb637cf5922f92f3
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-08-02 22:43:05 +0530

    fix: add VscCode and SiDotnet to icon map, update fallback icons

--- src/lib/icon-map.ts
+++ src/lib/icon-map.ts
@@ -6,9 +6,10 @@ import {
 import {
   SiRedux, SiTailwindcss, SiTypescript, SiNextdotjs, SiPrisma,
   SiPostgresql, SiMongodb, SiFirebase, SiGraphql, SiRedis,
-  SiDocker, SiVercel, SiNetlify, SiFigma,
+  SiDocker, SiVercel, SiNetlify, SiFigma, SiDotnet,
 } from "react-icons/si"
 import { DiDotnet, DiMsqlServer, DiDatabase, DiJava, DiRuby } from "react-icons/di"
+import { VscCode } from "react-icons/vsc"
 import {
   TbDatabase, TbServer, TbBrandVscode, TbBrandVisualStudio,
   TbBrandFirebase, TbBrandDocker, TbBrandAws,
@@ -22,8 +23,9 @@ export const iconMap: Record<string, React.ComponentType<any>> = {
   FaFigma, FaAws, FaGoogle, FaNpm, FaYarn,
   SiRedux, SiTailwindcss, SiTypescript, SiNextdotjs, SiPrisma,
   SiPostgresql, SiMongodb, SiFirebase, SiGraphql, SiRedis,
-  SiDocker, SiVercel, SiNetlify, SiFigma,
+  SiDocker, SiVercel, SiNetlify, SiFigma, SiDotnet,
   DiDotnet, DiMsqlServer, DiDatabase, DiJava, DiRuby,
+  VscCode,
   TbDatabase, TbServer, TbBrandVscode, TbBrandVisualStudio,
   TbBrandFirebase, TbBrandDocker, TbBrandAws,
   TbBrandGit, TbBrandGithub, TbBrandReact, TbBrandNodejs,
