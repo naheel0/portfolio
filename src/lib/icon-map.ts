c29bc111643cff81d8a7ced6c9eea655e4561b14
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-08-16 00:48:52 +0530

    chore: add 11 missing icon-map entries for skill icons
    Adds SiBlazor, SiNuget, SiSwagger, SiGithubactions, SiRabbitmq,
    SiApachekafka, SiOpenapiinitiative, VscVscode, TbBrandAzure,
    TbBrandPowershell, TbBrandLinqpad to match admin icon-map.

--- src/lib/icon-map.ts
+++ src/lib/icon-map.ts
@@ -6,14 +6,16 @@ import {
 import {
   SiRedux, SiTailwindcss, SiTypescript, SiNextdotjs, SiPrisma, SiPostgresql,
   SiMongodb, SiFirebase, SiGraphql, SiRedis, SiDocker, SiVercel, SiNetlify,
-  SiFigma, SiDotnet, SiVite, SiRazorpay,
+  SiFigma, SiDotnet, SiVite, SiRazorpay, SiBlazor, SiNuget, SiSwagger,
+  SiGithubactions, SiRabbitmq, SiApachekafka, SiOpenapiinitiative,
 } from "react-icons/si"
 import { DiDotnet, DiDatabase, DiMsqlServer, DiJava, DiRuby } from "react-icons/di"
-import { VscCode } from "react-icons/vsc"
+import { VscCode, VscVscode } from "react-icons/vsc"
 import {
   TbDatabase, TbServer, TbBrandVscode, TbBrandVisualStudio, TbBrandFirebase,
   TbBrandDocker, TbBrandAws, TbBrandGit, TbBrandGithub, TbBrandReact,
   TbBrandNodejs, TbBrandPython, TbBrandTypescript, TbBrandCSharp, TbLock,
+  TbBrandAzure, TbBrandPowershell, TbBrandLinqpad,
 } from "react-icons/tb"
 
 export const iconMap: Record<string, React.ComponentType<any>> = {
@@ -22,12 +24,14 @@ export const iconMap: Record<string, React.ComponentType<any>> = {
   FaUserTie, FaCloud, FaLock, FaUsers,
   SiRedux, SiTailwindcss, SiTypescript, SiNextdotjs, SiPrisma, SiPostgresql,
   SiMongodb, SiFirebase, SiGraphql, SiRedis, SiDocker, SiVercel, SiNetlify,
-  SiFigma, SiDotnet, SiVite, SiRazorpay,
+  SiFigma, SiDotnet, SiVite, SiRazorpay, SiBlazor, SiNuget, SiSwagger,
+  SiGithubactions, SiRabbitmq, SiApachekafka, SiOpenapiinitiative,
   DiDotnet, DiDatabase, DiMsqlServer, DiJava, DiRuby,
-  VscCode,
+  VscCode, VscVscode,
   TbDatabase, TbServer, TbBrandVscode, TbBrandVisualStudio, TbBrandFirebase,
   TbBrandDocker, TbBrandAws, TbBrandGit, TbBrandGithub, TbBrandReact,
   TbBrandNodejs, TbBrandPython, TbBrandTypescript, TbBrandCSharp, TbLock,
+  TbBrandAzure, TbBrandPowershell, TbBrandLinqpad,
 }
 
 export const DefaultIcon = FaCode;
